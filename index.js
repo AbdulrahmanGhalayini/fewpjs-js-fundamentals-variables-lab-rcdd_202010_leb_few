9e16c435628e82b02338decb7a8a3ed9927f1c35
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2020-12-05 13:33:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 const companyName= 'Scuber';
 
 let companyCeo= 'Susan Smith';
 let mostProfitableNeighborhood='Chelsea'
+=======
+let companyName= 'Scuber';
+
+let companyCeo= 'Susan Smith';
+>>>>>>> a3fee6f2edd0ac4bb71fe61aaa62142d83a976e7
